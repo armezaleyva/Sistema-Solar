7ec1cf0ab52e380db21150aa9eb32b4b523fd995
Armando Meza Leyva <armezaleyva@gmail.com>
2019-02-06 08:42:06 -0700

    Se agregó el péndulo

---
+++ Assets/Pendulo/Assets/UserInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool inputActivo = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inputActivo = !inputActivo;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (inputActivo)
+        {
+            float movimientoMouse = Input.GetAxis("Mouse X");
+            transform.Translate(new Vector3(1, 0, 0) * movimientoMouse);
+        }
+    }
+}
